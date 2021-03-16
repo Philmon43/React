56b085d7215d416d32c1942f1429f147755812e2
philmon43 <philmon43@gmail.com>
2021-03-16 19:47:50 +0200

    quiz

---
+++ quiz/src/App.js
@@ -0,0 +1,11 @@
+import Quiz from "./Components/Quiz";
+
+const App = () => {
+  return (
+    <div>
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
---
+++ quiz/src/Components/Quiz.js
@@ -0,0 +1,13 @@
+const Quiz = ()=>{
+    return (
+        <div>
+            <h2>How do you Like Front End?</h2>
+            <p>how much do you like it?</p>
+            <input style={{display: "block"}} type="range" />
+            <input type="text-area"></input>
+        </div>
+    )
+
+}
+
+export default Quiz
