b705ee7013fab73601e5842ce2465d5b2bb8b264
philmon43 <philmon43@gmail.com>
2021-03-16 12:11:16 +0200

    react increament number ex

--- appleseeds-react/src/App.js
+++ appleseeds-react/src/App.js
@@ -1,28 +1,55 @@
 import React from "react"
-import Cards from "./Components/Cards/Cards"
 
-class App extends React.Component{
-    render(){
-        return(
-            <div>
-                <Cards
-                    title= "My first Card"
-                    source="https://placeimg.com/640/480/any"
-                    paragraph="Random generted images"
-                />
-                <Cards
-                    title= "My second Card"
-                    source="https://placeimg.com/640/480/any"
-                    paragraph="Random generted images 2nd"
-                />
-                <Cards
-                    title= "My second Card"
-                    source="https://placeimg.com/640/480/any"
-                    paragraph="Random generted images 2nd"
-                />
+//cards class component
+// class App extends React.Component{
+//     render(){
+//         return(
+//             <div>
+//                 <Cards
+//                     title= "My first Card"
+//                     source="https://placeimg.com/640/480/any"
+//                     paragraph="Random generted images"
+//                 />
+//                 <Cards
+//                     title= "My second Card"
+//                     source="https://placeimg.com/640/480/any"
+//                     paragraph="Random generted images 2nd"
+//                 />
+//                 <Cards
+//                     title= "My second Card"
+//                     source="https://placeimg.com/640/480/any"
+//                     paragraph="Random generted images 2nd"
+//                 />
+//             </div>
+//         )
+//     }
+// }
+
+
+//********************************************************************
+//********************************************************************
+//********************************************************************
+
+//increament
+class Counter extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = { value: 0 };
+      }
+      handleCounter() {
+        this.setState({ value: this.state.value + 1 });
+      }
+      render(){
+          return(
+              <div>
+                <button
+                    onClick={this.handleCounter.bind(this)}
+                >counter</button> 
+                <span> {this.state.value}</span>
             </div>
-        )
-    }
+          )
+      }
 }
 
-export default App;
+
+export default Counter;
