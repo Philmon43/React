3fb938dfd900db8ac6833eae0abeb74838a5f790
philmon43 <philmon43@gmail.com>
2021-03-16 11:23:55 +0200

    react class component on Cards

--- appleseeds-react/src/App.js
+++ appleseeds-react/src/App.js
@@ -1,25 +1,28 @@
+import React from "react"
 import Cards from "./Components/Cards/Cards"
 
-const App = () => {
-    return (
-        <div>
-            <Cards
-                title= "My first Card"
-                source="https://placeimg.com/640/480/any"
-                paragraph="Random generted images"
-            />
-            <Cards
-                title= "My second Card"
-                source="https://placeimg.com/640/480/any"
-                paragraph="Random generted images 2nd"
-            />
-            <Cards
-                title= "My second Card"
-                source="https://placeimg.com/640/480/any"
-                paragraph="Random generted images 2nd"
-            />
-        </div>
-    )
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Cards
+                    title= "My first Card"
+                    source="https://placeimg.com/640/480/any"
+                    paragraph="Random generted images"
+                />
+                <Cards
+                    title= "My second Card"
+                    source="https://placeimg.com/640/480/any"
+                    paragraph="Random generted images 2nd"
+                />
+                <Cards
+                    title= "My second Card"
+                    source="https://placeimg.com/640/480/any"
+                    paragraph="Random generted images 2nd"
+                />
+            </div>
+        )
+    }
 }
 
 export default App;
--- appleseeds-react/src/Components/Cards/Cards.js
+++ appleseeds-react/src/Components/Cards/Cards.js
@@ -1,15 +1,21 @@
+import React from "react";
 import "./Cards.css";
 
-const Card = ({title, paragraph, source}) => {
-    return <div className="card">
-                <img src={source} alt="ant_image" />
+class Card extends React.Component{
+    render(){
+        return (
+            <div className="card">
+                <img src={this.props.source} alt="ant_image" />
                 <div className="card_details">
-                    <h1>{title}</h1>
-                    <p>{paragraph}</p>
+                    <h1>{this.props.title}</h1>
+                    <p>{this.props.paragraph}</p>
                     <a href="#">SHARE</a>
                     <a href="#">EXPLORE</a>
                 </div>
             </div>
+        )
+    }
 }
+
 
 export default Card
