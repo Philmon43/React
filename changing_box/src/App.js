6e28472a83593764275be61c01235cf52af75164
philmon43 <philmon43@gmail.com>
2021-03-17 15:15:13 +0200

    changing_box

---
+++ changing_box/src/App.js
@@ -0,0 +1,26 @@
+import React from "react"
+import "./app.css"
+export default class App extends React.Component{
+    state = {
+        counter: 0,
+        colors: ["indianred", "green", "yellow", "dodgerblue", "orange"],
+        currentColor: function(){return this.colors[this.counter]},
+        borderRadius: function(){return this.counter===5?"50%": ""}
+    }
+
+    componentDidMount(){
+       setInterval(() => {
+           if(this.state.counter<5){
+                this.setState({counter: this.state.counter+1})
+           }else{
+                window.clearInterval()
+           }
+       },500)
+    }
+    render(){
+        console.log(this)
+        return (
+            <div className="box" style={{background: this.state.currentColor(), borderRadius:this.state.borderRadius()}}></div>
+        )
+    }
+}
